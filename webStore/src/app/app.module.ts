39d62865d1cda846eeb28d9930fdf731d4e18a79
rubendario981 <rubendario981@hotmail.com>
2020-07-28 20:02:36 -0500

    se inicia proyecto con angular creando los primeros componentes de la aplicacion en la pagina principal

--- webStore/src/app/app.module.ts
+++ webStore/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { ListadoProductosComponent } from './componentes/listado-productos/listado-productos.component';
+import { DescripcionProductoComponent } from './componentes/descripcion-producto/descripcion-producto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegacionComponent,
+    ListadoProductosComponent,
+    DescripcionProductoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ webStore/src/app/componentes/descripcion-producto/descripcion-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descripcion-producto',
+  templateUrl: './descripcion-producto.component.html',
+  styleUrls: ['./descripcion-producto.component.css']
+})
+export class DescripcionProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

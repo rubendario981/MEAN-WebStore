1ae4198dfeeee1352df920c6437808ba0f06ef7f
rubendario981 <rubendario981@hotmail.com>
2020-09-03 10:00:27 -0500

    Se guardan cambios para iniciar nueva version para el registro e inicio de session con passport

--- backend/sessionPassport.js
+++ backend/sessionPassport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = require('../models/user');
+const User = require('./modeloUsuario');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -12,23 +12,33 @@ passport.deserializeUser(async (id, done) => {
   done(null, user);
 });
 
-passport.use('local-signup', new LocalStrategy({
-  usernameField: 'email',
+passport.use('registro', new LocalStrategy({
+  usernameField: 'correo',
   passwordField: 'password',
-  passReqToCallback: true
-}, async (req, email, password, done) => {
-  const user = await User.findOne({'email': email})
-  console.log(user)
-  if(user) {
+  passReqToCallback: true }, 
+  async (req, correo, password, done) => {
+  //const user = await User.findOne({'correo': correo })
+  //console.log(user)
+  //user ? done(null, false, ): 
+  newUser = new User()
+  newUser.email = email;
+  newUser.password = newUser.validaPass(password);
+  newUser.nombres = req.params.nombres;
+  newUser.nickName = req.params.nickName;
+  newUser.rol = req.params.rol;
+  //console.log(newUser)
+  await newUser.save();
+  done(null, newUser);
+  /* if (user) {
     return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'));
   } else {
     const newUser = new User();
     newUser.email = email;
     newUser.password = newUser.encryptPassword(password);
-  console.log(newUser)
+    console.log(newUser)
     await newUser.save();
     done(null, newUser);
-  }
+  } */
 }));
 
 passport.use('local-signin', new LocalStrategy({
@@ -36,11 +46,11 @@ passport.use('local-signin', new LocalStrategy({
   passwordField: 'password',
   passReqToCallback: true
 }, async (req, email, password, done) => {
-  const user = await User.findOne({email: email});
-  if(!user) {
+  const user = await User.findOne({ email: email });
+  if (!user) {
     return done(null, false, req.flash('signinMessage', 'No User Found'));
   }
-  if(!user.comparePassword(password)) {
+  if (!user.comparePassword(password)) {
     return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
   }
   return done(null, user);
